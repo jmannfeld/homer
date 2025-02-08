8080c47319d890d37db9c98fe5e50fb3be3990cc
Jordan Mannfeld <jordanmannfeld@gmail.com>
2025-02-07 20:41:19 -0500

    handles case if there are no branches

--- commands/tag.js
+++ commands/tag.js
@@ -4,9 +4,20 @@ export default async function tagCommand(options) {
   const git = simpleGit();
 
   try {
+    // get the current branch
     const branchSummary = await git.branch();
-    console.log("branchSummary", branchSummary);
-    console.log("Current branch:", branchSummary.current);
+    console.log(branchSummary);
+    if (branchSummary.current) {
+      console.log("Current branch:", branchSummary.current);
+    } else {
+      console.error("❌ Error: Unable to determine the current branch.");
+      console.error(
+        "🔹 Tip: Make sure a commit has been made in the Git repository."
+      );
+    }
+    // get the latest tag from the current branch
+    const tags = await git.tags();
+    console.log(tags);
   } catch (error) {
     console.error("❌ Error: Unable to determine the current branch.");
     console.error("🔹 Tip: Make sure you are inside a Git repository.");
